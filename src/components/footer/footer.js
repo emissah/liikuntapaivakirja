69b211b10252478eb9ce631f9eaca68cd00e06e9
emissah <emilia.nuutila@gmail.com>
2022-08-01 18:28:00 +0300

    Footer component added

---
+++ src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import styles from './footer.module.scss';
+
+function Footer(props) {
+return (
+  <div className={styles.footer}>
+   <p>Kuva Uplash-sivustolta</p> 
+  </div>
+)};
+
+export default Footer; 
