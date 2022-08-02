a01c1de7b75ee9105811772c41b5b40d1f47eb52
emissah <emilia.nuutila@gmail.com>
2022-08-02 20:31:21 +0300

    Testdata updated

--- src/testdata.js
+++ src/testdata.js
@@ -2,10 +2,10 @@ const data = [
     {
         id: "1",
         type:   "Juoksu",
-        kilometres:  "5",
+        kilometres:  5,
         date:  "2021-11-10",
-        hours: "0",
-        minutes:  "30",
+        hours: 0,
+        minutes:  30,
         periodStart: "2021-11-01",
         periodEnd: "2021-11-31"
 
@@ -14,30 +14,30 @@ const data = [
      {
         id: "2",
         type: "Tanssi",
-        kilometres: "0",
+        kilometres: 0,
         date: "2021-12-12",
-        hours: "0",
-        minutes: "20",
+        hours: 0,
+        minutes: 20,
         periodStart: "2021-12-01",
         periodEnd: "2021-12-31"
     }, 
     {
         id: "3",
         type: "Pyöräily",
-        kilometres: "12",
+        kilometres: 12,
         date: "2021-10-25",
-        hours: "0",
-        minutes: "30",
+        hours: 0,
+        minutes: 30,
         periodStart: "2021-10-01",
         periodEnd: "2021-10-31"
     },
     {
         id: "4",
         type: "Kävely",
-        kilometres: "4",
+        kilometres: 4,
         date: "2022-07-28",
-        hours: "1",
-        minutes: "5",
+        hours: 1,
+        minutes: 5,
         periodStart: "2022-07-01",
         periodEnd: "2022-07-31"
     }
